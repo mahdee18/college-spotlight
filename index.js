7172e77061c6e962df9f476cff762fe5822142fa
mahdee18 <mahdeec10@gmail.com>
2023-07-24 23:18:42 +0600

    Added vercel json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 4000
 
@@ -38,7 +38,13 @@ async function run() {
             res.send(result)
             
         })
-
+        // Get Details
+        app.get("/cardData/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await cardCollection.findOne(query)
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
